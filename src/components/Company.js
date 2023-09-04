fe5060b75c3665468b93b4b379c65db52224901b
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-09-04 11:43:52 +0200

    Create store and slice

---
+++ src/components/Company.js
@@ -0,0 +1,43 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchStockScreener, selectCompany } from '../redux/stockScreener/StockScreenersSlice';
+import Search from './Search';
+import './styles/Company.css';
+
+function Company() {
+  const dispatch = useDispatch();
+  const stockScreeners = useSelector((state) => state.stockScreeners);
+
+  useEffect(() => {
+    dispatch(fetchStockScreener());
+  }, [dispatch]);
+
+  return (
+    <div className="navigation">
+      <ul className="nav-bar">
+        <li className="subject">Stock Screener</li>
+        <li><Search /></li>
+      </ul>
+      <div className="companyContainer">
+        {stockScreeners.searchStockCompany.map((screener) => (
+          <div key={screener.symbol}>
+            <Link
+              to="/Screeners"
+              onClick={() => dispatch(selectCompany(screener))}
+            >
+              <ul className="company-card" key={screener.symbol}>
+                <li style={{ fontFamily: 'stencil Std, fantasy', fontSize: '24px' }}>{screener.symbol}</li>
+                <li style={{ marginBottom: '50px' }}>{screener.country}</li>
+                <li style={{ fontFamily: 'Chalkduster, fantasy' }}>{screener.companyName}</li>
+                <li>{screener.sector}</li>
+              </ul>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Company;
